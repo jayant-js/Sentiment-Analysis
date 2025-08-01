98251bc3428cf013374260770254d4d9bac01ff1
Jayant Singhal <jayantsinghal992@gmail.com>
2025-08-01 08:21:45 +0530

    End to End application ready

--- frontend/main.py
+++ frontend/main.py
@@ -7,18 +7,18 @@ if st.button('Predict'):
     if text:
         with st.spinner('Preprocessing the Input...'):
             response = requests.post(f'http://localhost:8000/input-text', json={'text': text})
-            if response.status_code == 200: 
-                with st.spinner('Getting sentiment...'):
-                    response = requests.get('http://localhost:8000/get-sentiment')
-                    if response.status_code == 200:
-                        sentiment = response.json().get('sentiment')
-                        if sentiment == 1:
-                            st.write('Positive')
-                        elif sentiment == 0:
-                            st.write('Negative')
-                    else:
-                        raise HTTPException(status_code=500)
-            else:
-                raise HTTPException(status_code=500)
+        if response.status_code == 200: 
+            with st.spinner('Getting sentiment...'):
+                response = requests.get('http://localhost:8000/get-sentiment')
+                if response.status_code == 200:
+                    sentiment = response.json().get('sentiment')
+                    if sentiment == 1:
+                        st.write('Positive')
+                    elif sentiment == 0:
+                        st.write('Negative')
+                else:
+                    st.error('Error while getting sentiment.')
+        else:
+            st.error('Error during preprocessing')
     else:
-        raise ValueError('Give correct input')
+        st.warning('Please enter some text')
